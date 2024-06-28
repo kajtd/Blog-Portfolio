b2b4b4906c163c31e2dfe32b69a905b3f8f65014
Kajetan Domagala <kajtekd8@gmail.com>
2024-06-28 15:37:18 +0200

    Update loader looks

--- components/App/Loader.vue
+++ components/App/Loader.vue
@@ -3,15 +3,7 @@
     class="fixed inset-0 bg-white/90 flex items-center justify-center z-[150] text-9xl font-black"
   >
     <div
-      class="border-2 border-primary-400 rounded-full w-10 h-10 animate-[2s_pulse_infinite] flex justify-center items-center"
-    >
-      <div
-        class="border-2 border-primary-400 rounded-full w-5 h-5 animate-[1s_pulse_infinite] flex justify-center items-center"
-      >
-        <div
-          class="border-2 border-primary-400 rounded-full w-1 h-1 flex justify-center items-center animate-none"
-        ></div>
-      </div>
-    </div>
+      class="border-2 border-primary-400 rounded shadow-2xl w-10 h-10 animate-[2s_spin_infinite] flex justify-center items-center"
+    ></div>
   </div>
 </template>
