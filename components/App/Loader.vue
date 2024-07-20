0843282a7e88a65b492911424caf3a09b6760f59
Kajetan Domagala <kajtekd8@gmail.com>
2024-07-20 08:44:13 +0200

    :hammer: make loader smaller

--- components/App/Loader.vue
+++ components/App/Loader.vue
@@ -3,7 +3,7 @@
     class="fixed inset-0 bg-white/90 flex items-center justify-center z-[150] text-9xl font-black"
   >
     <div
-      class="border-2 border-primary-400 rounded shadow-2xl w-10 h-10 animate-[2s_spin_infinite] flex justify-center items-center"
+      class="border-2 border-primary-400 rounded shadow-2xl w-4 h-4 animate-[2s_spin_infinite] flex justify-center items-center"
     ></div>
   </div>
 </template>
